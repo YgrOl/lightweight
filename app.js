af434dbe815a4f5efdbcde7f4e7477f933e1cf29
Sasha <110664032+YgrOl@users.noreply.github.com>
2022-12-22 22:59:11 +0200

    2лаба

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-import express from 'express';
+import express from 'express'
 
-const PORT = 3000;
-const server = express();
+const PORT = 3000
+const server = express()
 
-console.log ("Hello, world!");
-const message = "Hello node.js";
-console.log(message);
+
+console.log ("Hello, world!")
+const message = "Hello node.js"
+console.log(message)
+
+server.listen(PORT, () => {
+    console.log('Server has been started on port '+ PORT)
+})
+
+
+
+
 
 server.get('/',(req, res) =>{
     res.send("Hello my js-route")
 })
 
-server.listen(PORT, () => {
-    console.log('Server has been started on port '+ PORT)
-})
+
 
