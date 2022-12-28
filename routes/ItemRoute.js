b4d3d7bb2d1d2240b7f6d59cec0d1af8c4efde35
Sasha <110664032+YgrOl@users.noreply.github.com>
2022-12-28 13:02:50 +0200

    5лаба

--- routes/ItemRoute.js
+++ routes/ItemRoute.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import _ from 'lodash';
 import  details from '../data/details.json'
+import mongoose from 'mongoose';
+
+
+
+const DB_URL = 'mongodb://localhost:27017/lightW';
+const DB_USER = '';
+const DB_password = '';
+
+
 
 
 const router = express.Router();
 
 
 let detailsArray = details;
+
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('DB CONNECTION IS ESTABLISHED')
+});
 
 
 
